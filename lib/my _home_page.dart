de10e31d8d3cfcadfddc6371be9ea26cae00feb0
simax1721 <simax1721@gmail.com>
2023-02-28 15:35:18 +0700

    z

--- lib/my _home_page.dart
+++ lib/my _home_page.dart
@@ -13,22 +13,48 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xff00ff00),
       ),
-      body: Container(
-        height: double.infinity,
-        width: MediaQuery.of(context).size.width,
-        color: const Color(0xffff00ff),
-        child: const Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
+      body: Row(
+        children: [
+          Flexible(
+            child: Container(
+              // height: double.infinity,
+              height: MediaQuery.of(context).size.width * 0.5,
+              width: MediaQuery.of(context).size.width,
+              color: const Color(0xffff00ff),
+              child: const Center(
+                child: Text(
+                  "Hello World",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  ),
+                // color: Colors.white,
+                ),
             ),
-            ),
-          // color: Colors.white,
-          ),
       ),
+      SizedBox( height: 10, ),
+      SizedBox(
+        height: 50,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: ElevatedButton(
+          onPressed: () {}, 
+          child: Text("Tekan"),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            elevation: 2,
+            // padding: EdgeInsets.symmetric(
+            //   horizontal: 30,
+            //   vertical: 25
+            // ),
+          ),
+          ),
+      )
+      ],) 
     );
   }
 }
